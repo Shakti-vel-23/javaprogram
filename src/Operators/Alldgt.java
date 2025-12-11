e48c5364c36d392e4bdac329d02d33730c34c59b
SHAKT <shaktivel230@gmail.com>
2025-12-11 20:43:37 +0530

    commit -m

---
+++ src/Operators/Alldgt.java
@@ -0,0 +1,10 @@
+package Operators;
+import java.util.*;
+public class Alldgt {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        int n = a.nextInt();
+        int k = n / 10;
+        System.out.println(k);
+    }
+}
