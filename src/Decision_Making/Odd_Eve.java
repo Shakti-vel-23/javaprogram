a29cb95235dfbd2517df0b75ffa8bf04039fd1ec
SHAKT <shaktivel230@gmail.com>
2025-12-12 10:17:49 +0530

    commit -m

---
+++ src/Decision_Making/Odd_Eve.java
@@ -0,0 +1,12 @@
+package Decision_Making;
+import java.util.Scanner;
+public class Odd_Eve {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%2==0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
