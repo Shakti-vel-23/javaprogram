80b7a7daaef550416e6166ffc3847eb9d846ef7a
SHAKT <shaktivel230@gmail.com>
2025-12-12 14:38:20 +0530

    commit -m

---
+++ src/Operators/Mid_no.java
@@ -0,0 +1,9 @@
+package Operators;
+import java.util.*;
+public class Mid_no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s.charAt(1));
+    }
+}
