d86a662b74c051418cad8dc37cf9c3ed267e288d
Shakti <shaktivel230@gmail.com>
2025-12-12 22:48:56 +0530

    commit -m

---
+++ src/Operators/Area_Rect.java
@@ -0,0 +1,10 @@
+package Operators;
+import java.util.*;
+public class Area_Rect {
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        int length = a.nextInt();
+        int width = a.nextInt();
+        System.out.println(length*width);
+    }
+}
---
+++ src/Operators/Ctof.java
@@ -0,0 +1,8 @@
+package Operators;
+class Ctof {
+    public static void main(String[] args) {
+        double c = 37;
+        double f = (c * 9 / 5) + 32;
+        System.out.println(f);
+    }
+}
