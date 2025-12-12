5006c6a5f46030034d15fb2e388bc657c0255372
SHAKT <shaktivel230@gmail.com>
2025-12-12 09:21:51 +0530

    commit -m

---
+++ src/Operators/Sfl.java
@@ -0,0 +1,11 @@
+package Operators;
+import java.util.*;
+public class Sfl {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n/100;//first
+        int b = n%10;
+        System.out.println(a+b);
+    }
+}
